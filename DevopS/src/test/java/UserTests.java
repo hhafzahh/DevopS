8179dc714baa1f78851fa4d58943c81228014f25
hhafzahh <94510297+hhafzahh@users.noreply.github.com>
2022-02-26 14:08:28 +0800

    Created more test cases using testng.xml
    * Created new branch & xml file
    
    * Update build.xml file
    
    * Update Pom.xml
    
    * Added Some TestNg Test Cases
    
    * Update Some tests & added some name attribute to jsp files
    
    * Update project with test cases
    
    * Update ChromeDriver
    
    * Update chromedriver
    
    * Create build.yml
    
    * Update pom.xml
    
    * Delete build.yml

---
+++ DevopS/src/test/java/UserTests.java
@@ -0,0 +1,51 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class UserTests {
+	private WebDriver webDriver;
+	 @BeforeTest
+	  public void beforeTest() {
+		//Setting system properties of ChromeDriver
+		  //to amend directory path base on your local file path
+		  String chromeDriverDir = "C:\\Users\\Hafsah\\chromedriver.exe";
+
+		  System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+
+		  ///initialize FirefoxDriver at the start of test
+		  webDriver = new ChromeDriver();  
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+		  webDriver.close();  
+	}
+
+  @Test
+  public void listUsersTest() {
+	  webDriver.navigate().to("http://localhost:8090/DevopS/UserServlet/dashboard");
+	  String Heading = webDriver.findElement(By.name("listOfUsers")).getAttribute("innerHTML");
+	  Assert.assertTrue(Heading.contains("List of Users"));
+  }
+
+  @Test
+  public void showEditFormTest() {
+	    webDriver.navigate().to("http://localhost:8090/DevopS/UserServlet/dashboard");
+		webDriver.findElement(By.xpath("//tbody//tr//td//a")).click();
+		System.out.println(webDriver.getCurrentUrl());
+		System.out.println(webDriver.getPageSource());
+		String Title = webDriver.findElement(By.name("edit")).getAttribute("innerHTML");
+		Assert.assertTrue(Title.contains("Edit User"));
+  }
+
+ 
+}
